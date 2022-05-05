ac61e05585f95a0305fec98f3d584a270228680d
itstheanurag <gauravanurag36@gmail.com>
2022-05-05 23:26:27 +0530

    Final Submission

--- src/controllers/collegeControllers.js
+++ src/controllers/collegeControllers.js
@@ -6,44 +6,44 @@ const createCollege = async (req,res)=>{
     try{
         let data = req.body
 
-        const {name, fullName, logoLink} = data
+        const {name, fullName, logoLink} = data //destructuring required fields
 
         if(!name){
             return res.status(400).send({status : false, msg : "name is a required field"})
-        }
+        } // in case name is not given
 
-        const namePattern = /^[a-z]((?![? .,'-]$)[ .]?[a-z]){1,10}$/g
+        const namePattern = /^[a-z]((?![? .,'-]$)[ .]?[a-z]){1,10}$/g  //creating pattern for valid name
         
         if(!name.match(namePattern)){
             return res.status(400).send({status : false, msg : "This is not a valid Name"})
-        }
+        } // checking if the name is valid
 
         if(!fullName){
             return res.status(400).send({status : false, msg : "fullName is a required field"})
-        }
+        } //in case full name is not present
 
-        const fullNamePattern = /^[a-z]((?![? .,'-]$)[ .]?[a-z]){3,150}$/gi
+        const fullNamePattern = /^[a-z]((?![? .,'-]$)[ .]?[a-z]){3,150}$/gi  //creating pattern for valid fullname -case insentitive
         
         if(!fullName.match(fullNamePattern)){
             return res.status(400).send({status : false, msg : "This is not a valid full Name"})
-        }
+        } //valdiating the full name
 
         if(!logoLink){
             return res.status(400).send({status : false, msg : "logoLink is a required field"})
-        }
+        } //if the logoLink is not present 
 
-        const validateLogoLink = url.isUri(logoLink)
+        const validateLogoLink = url.isUri(logoLink)  //using valid-url to valdiate the email
 
         if(!validateLogoLink){
         return res.status(400).send({status : false, msg : "This is not a valid logoLink"})
-        }
+        } //in case email validation fails
 
-        if (!await collegModel.exists({name : data.name})){
-            let college = await collegModel.create(data)
-            return res.status(201).send({status : true, msg : "Your college has been registered", data : college })
+        if (!await collegModel.exists({name : data.name})){ //in case this college already does not exists
+            let college = await collegModel.create(data) // creating a new college document
+            return res.status(201).send({status : true, msg : "Your college has been registered", data : college }) //sending the response
         }else{
             return res.status(400).send({status : false, msg : "this college name is already registered"})
-        }
+        } //response in case the data already exists in db
     }
     catch (err){
         return res.status(500).send({status : false, err : err.message})
@@ -60,40 +60,41 @@ const createCollege = async (req,res)=>{
 const collegeDetails = async (req,res)=>{
     try {
         let data = req.query
-        let {collegeName} = data
+        let {collegeName} = data //destructuring the collegeName from the data
          
         if(!collegeName){
              return res.status(400).send({status : false, msg : "College Name is required to perform this action"})
-         }
+         } //response in case college name is not present
 
-        let namePattern = /^[a-z]((?![? .,'-]$)[ .]?[a-z]){1,10}$/g
+        let namePattern = /^[a-z]((?![? .,'-]$)[ .]?[a-z]){1,10}$/g //creating a valid name pattern
         
         if(!collegeName.match(namePattern)){
              return res.status(400).send({status : false, msg : "This is not a valid college Name"})
-        }
+        } // matching the name we got in the request again the valid pattern and the response if it fails. 
+        // in case it matches then we will try to find the college with that name we were given in the request
         
-        let findcollege = await collegModel.findOne({name : collegeName, isDeleted : false}).select({_id : 1, name : 1, logoLink: 1, fullName: 1})
+        let findcollege = await collegModel.findOne({name : collegeName, isDeleted : false}).select({_id : 1, name : 1, logoLink: 1, fullName: 1}) //selecting the required fields
 
         if(!findcollege){
             return res.status(400).send({ status: false, msg: "No college with this name exists" })
-        }
+        } // in caase no college was found, this would be our response
 
-        let collegeId = findcollege._id
+        let collegeId = findcollege._id //storing the id of the college with find from the name inside a variable collegeId
 
         let candidates = await internModel.find({collegeId : collegeId, isDeleted : false}).select({name : 1, email : 1, mobile : 1})
-        
+        //searching for candidates inside the intern model and selecting their name, email, and mobiles
         if(!candidates.length){
             return res.status(400).send({ status: false, msg: "no students from this college has applied yet" })
-        }
+        } // since find gives us an array, we can check for its length and in case it's zero, this would be our response
 
         let details = {
             name : findcollege.name,
             fullName : findcollege.fullName,
             logoLink : findcollege.logoLink,
             intrests : candidates
-        }
+        } //creating a details object with all the required keys and values
 
-        return res.status(200).send({ status: true, data: details })
+        return res.status(200).send({ status: true, data: details }) //sending newly created details document
 
     } catch (error) {
         
--- src/controllers/internControllers.js
+++ src/controllers/internControllers.js
@@ -7,85 +7,88 @@ const validator = require ("email-validator")
 const createIntern = async (req,res)=>{
     try{
         const data = req.body
-        const { name, mobile, email, collegeName} = data
-
-        if(!email){
-            return res.status(400).send({status : false, msg : "email is a required field"})
-        }
+        const { name, mobile, email, collegeName} = data // destructuring the required fields from data
 
         if(!name){
             return res.status(400).send({status : false, msg : "name is required field"})
-        }
+        } // response in case no name as been present
 
-        let namePattern = /^[a-z]((?![? .,'-]$)[ .]?[a-z]){3,24}$/gi
+        let namePattern = /^[a-z]((?![? .,'-]$)[ .]?[a-z]){3,24}$/gi // creating a pattern for valid name with regex
         
-        if(!name.match(namePattern)){
+        if(!name.match(namePattern)){ //in case the name from the data does not match our pattern
             return res.status(400).send({status : false, msg : "This is not a valid Name"})
-        }
+        } // this will be our response
+
+        if(!email){
+            return res.status(400).send({status : false, msg : "email is a required field"})
+        } // our response in case no email has been provied
 /*
         const emailPattern = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})/
         if(!email.match(emailPattern)){
             return res.status(400).send({status : false, msg : "This is not a valid email"})
         }        //emial validations with regex
-*/
-        const validEmail = validator.validate(email)
+*/      
+        const validEmail = validator.validate(email) // using email-validator to validate the recieved email
         if(!validEmail){
             return res.status(400).send({status : false, msg : "this email is not valid"})
-        }
+        } // our response in case it's an invalid email
 
-        let findEmail = await internModel.findOne({email : email})
+        let findEmail = await internModel.findOne({email : email}) // in case our email is valid, then we check if it's already being used by some user
+
         if(findEmail){
             return res.status(400).send({status : false, msg : "a user with this email already exist"})
-        }
+        } // in case we found an intern with this email, this will be our response
         
-        if(!mobile){
+        if(!mobile){ // in case no mobile number has been passed
             return res.status(400).send({status : false, msg : "Mobile is a required field and can not be empty"})
         }
-        const mobiles = mobile.replace(/\s+/g, '')
+        const mobiles = mobile.replace(/\s+/g, '') // removing space from in between in case we recieved (+91 9876543212)
 
-        const mobilePattern = /^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[6789]\d{9}$/g
-        if(!mobiles.match(mobilePattern)){
+        const mobilePattern = /^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[6789]\d{9}$/g // declaring valid phone number pattern
+       
+        if(!mobiles.match(mobilePattern)){ // matching the number we recieved in data with the valid pattern
             return res.status(400).send({status : false, msg : "This is not a valid Mobile Number"})
-        } 
+        } // our response in case, the number does not match the valid pattern
 
-        let findMobile = await internModel.findOne({mobile : mobile})
+        let findMobile = await internModel.findOne({mobile : mobile}) // in case number is a valid one, then we try to check is it's already being used
+
         if(findMobile){
             return res.status(400).send({status : false, msg : "a user with this mobile already exist"})
-        }
+        } // our response in case the number is already being used by someone
         
         if(!collegeName){
             return res.status(400).send({status : false, msg : "college name is a required field"})
-        }
+        } // our response in case there was no college name provided
             
-        const findcollege = await collegeModel.findOne({name : collegeName})
+        const findcollege = await collegeModel.findOne({name : collegeName}) // checking for the college if any with that name exists
         if(!findcollege){
             return res.status(400).send({status : false, msg : "no college with this name exists"})
-        }
+        }// our response in case no college with that name exists
 
-        if(findcollege.isDeleted === true){
+        if(findcollege.isDeleted === true){ //checking if the found college is deleted or not
             return res.status(400).send({status : false, msg : "This college is no longer with us"})
-        }
+        } // our response in case the college with that name has been set to deleted
 
-        const collegeId = findcollege._id
+        const collegeId = findcollege._id // storing the id of the college we found in collegeId variable
 
         const internData = {
             name, 
             mobile,
             email,
             collegeId
-        }
+        } //creating new intern Data from all the above
 
-        const findIntern = await internModel.findOne(internData)
+        const findIntern = await internModel.findOne(internData) // trying to find an user with this new data in db
 
         if(findIntern){
             return res.status(400).send({status : false, msg : "student intern already exists"})
-        }
-        
-        const createIntern = await internModel.create(internData)
+        } // our response in case we found a student with the same data in db
+         
+        const createIntern = await internModel.create(internData) //creating a new intern in case we don not found any with the given data
 
         if(createIntern){
             return res.status(201).send({status : true, msg : "you have successfully registered", data : createIntern})
-        }
+        }// our response upon creating the intern successfully
 
     }
     catch(err){
